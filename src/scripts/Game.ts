7b2d1e28b854e79123dcbdda0bfc11c8cf0b62ea
kingBook <412126604@qq.com>
2021-01-07 16:12:00 +0800

    update 2021-01-07 16:11

--- src/scripts/Game.ts
+++ src/scripts/Game.ts
@@ -2,6 +2,10 @@ import BaseGame from "../framework/core/BaseGame";
 
 export default class Game extends BaseGame{
 	
+	private _levelNumber:number;
+	
+	public get levelNumber():number{ return this._levelNumber; }
+	
 	protected onAwake():void{
 		
 	}
@@ -11,10 +15,15 @@ export default class Game extends BaseGame{
 	}
 	
 	protected onStart():void{
-		/*Laya.Scene3D.load('res/unityExport/Conventional/Level.ls',Laya.Handler.create(null,function(scene:Laya.Scene3D):void{
-			Laya.stage.addChild(scene);
-		}));*/
+		
 	}
 	
+	public gotoLevel(levelNumber:number){
+		this._levelNumber=levelNumber;
+		
+		Laya.Scene3D.load('res/unityExport/Conventional/Level_1.ls',Laya.Handler.create(null,(scene:Laya.Scene3D)=>{
+			Laya.stage.addChild(scene);
+		}));
+	}
 	
 }
--- src/scripts/Level.ts
+++ src/scripts/Level.ts
@@ -1,8 +1,13 @@
+import App from "../framework/core/App";
 import Game from "./Game";
 
 export default class Level extends Laya.Script{
 	
 	protected _game:Game;
+	
+	protected onAwake():void{
+		this._game=App.instance.getGame<Game>();
+	}
 	
 	protected onStart():void{
 		
